8de698a07d521af4081488e44a6ff653e2600961
WK Wong <wingkwong.code@gmail.com>
2024-05-20 19:04:12 +0800

    refactor: apply eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,23 +2,24 @@ import type { AppProps } from "next/app";
 
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useRouter } from "next/router";
+
 import { fontSans, fontMono } from "@/config/fonts";
-import {useRouter} from 'next/router';
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
-	return (
-		<NextUIProvider navigate={router.push}>
-			<NextThemesProvider>
-				<Component {...pageProps} />
-			</NextThemesProvider>
-		</NextUIProvider>
-	);
+  return (
+    <NextUIProvider navigate={router.push}>
+      <NextThemesProvider>
+        <Component {...pageProps} />
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
 }
 
 export const fonts = {
-	sans: fontSans.style.fontFamily,
-	mono: fontMono.style.fontFamily,
+  sans: fontSans.style.fontFamily,
+  mono: fontMono.style.fontFamily,
 };
