0c1bea4a8426e4448d681691d4a8d4890c736d37
WK Wong <wingkwong.code@gmail.com>
2024-08-23 20:56:31 +0800

    fix(pages): revise div width

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function IndexPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
+        <div className="inline-block max-w-xl text-center justify-center">
           <h1 className={title()}>Make&nbsp;</h1>
           <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
           <br />
