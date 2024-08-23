34f72759bb80aef4106651d86b369df6121b88f3
WK Wong <wingkwong.code@gmail.com>
2024-08-23 20:56:18 +0800

    fix(pages): apply fontSans

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import clsx from "clsx";
+
+import { fontSans } from "@/config/fonts";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="min-h-screen bg-background font-sans antialiased">
+      <body
+        className={clsx(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
         <Main />
         <NextScript />
       </body>
